74a9ed65c99f2d7d952f815e13c873081dd7901f
charles <rmswo417@naver.com>
2024-05-11 20:42:43 +0900

    랜덤 번호 생성기 완료

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,7 +3,9 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:randomnumber/component/number_row.dart';
 import 'package:randomnumber/constant/color.dart';
+import 'package:randomnumber/screen/settings_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,16 +14,20 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+//번호를 랜덤으로 생성하기 위해 List int 타입으로 랜덤 번호를 넣어줬다.
 class _HomeScreenState extends State<HomeScreen> {
-  List<int> randomNumbers =[
+  int maxNumber = 1000;
+  List<int> randomNumbers = [
     123,
     456,
     789,
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: PRIMARY_COLOR,
+      //프라이마리 컬러 부분 이 부분은 color.dart 부분에서 색을 지정해줘서 쓸 수 있다.
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(
@@ -30,83 +36,127 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '랜덤 숫자 생성기 ',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.settings,
-                      color: RED_COLOR,
-                    ),
-                  ),
-                ],
+              _Header(
+                onPressed: onSettingsPop,
+              ), //코드 정리해서 보고 싶은 코드를 볼 수 있다.
+
+              _Body(
+                randomNumbers: randomNumbers,
               ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:
-                    randomNumbers
-                  .asMap()
-                  .entries
-                      .map(
-                        (x) => Padding(
-                          padding:  EdgeInsets.only(bottom: x.key == 2 ? 0:  16.0), //x값의 키가 2면 0을 실행 아니면 16을 실행
-                          child: Row(
-                            children: x.value
-                                .toString()
-                                .split('')
-                                .map(
-                                  (y) => Image.asset(
-                                    'asset/img/$y.png',
-                                    height: 70.0,
-                                    width: 50.0,
-                                  ),
-                                )
-                                .toList(),
-                          ),
-                        ),
-                      )
-                      .toList(),
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: RED_COLOR,
-                  ),
-                  onPressed: () {
-                    final rand = Random();
-
-                    final Set<int> newNumbers = {};
-
-                    while(newNumbers.length != 3){
-                     final number = rand.nextInt(1000);
-
-                     newNumbers.add(number);
-                    }
-
-
-                    setState(() {
-                      randomNumbers = newNumbers.toList();
-                    });
-
-                  },
-                  child: Text('생성하기'),
-                ),
-              ),
+              _Footer(onPressed: onRandomNumberGenerate),
             ],
           ),
         ),
+      ),
+    );
+  }
+  void onSettingsPop () async {
+    //list -- add
+    //[HomeScreen(), SettingsScreen()]
+    // 네비게이터는 of context는 바로 아래 오는 context를 찾아서 페이지를 이동시킨다.
+    final result = await Navigator.of(context).push<int>(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          return SettingsScreen(maxNumber: maxNumber,);
+        },
+      ),
+    );
+    if(result != null){
+      setState(() {
+        maxNumber = result;
+      });
+    }
+  }
+
+  void onRandomNumberGenerate() {
+    final rand = Random();
+
+    final Set<int> newNumbers = {};
+
+    while (newNumbers.length != 3) {
+      final number = rand.nextInt(maxNumber);
+
+      newNumbers.add(number);
+    }
+
+    setState(() {
+      randomNumbers = newNumbers.toList();
+    });
+  }
+}
+
+class _Header extends StatelessWidget {
+  final VoidCallback onPressed;
+  const _Header({required this.onPressed, Key? key,}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '랜덤 숫자 생성기 ',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+          onPressed:onPressed,
+          icon: Icon(
+            Icons.settings,
+            color: RED_COLOR,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNumbers;
+
+  const _Body({required this.randomNumbers, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: randomNumbers
+            .asMap()
+            .entries
+            .map(
+              (x) => Padding(
+                padding: EdgeInsets.only(bottom: x.key == 2 ? 0 : 16.0),
+                //x값의 키가 2면 0을 실행 아니면 16을 실행
+                child: NumberRow(
+                  number: x.value,
+                ),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _Footer({required this.onPressed, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: RED_COLOR,
+        ),
+        onPressed: onPressed,
+        child: Text('생성하기'),
       ),
     );
   }
